b0bcb61336a4cdbae61cd62368f99fe176b73529
Florian Popa <florian@webgenerals.com>
2018-08-14 09:37:16 +0300

    Initial commit

---
+++ src/main/java/SnippetsApp.java
@@ -0,0 +1,12 @@
+/**
+ * SnippetsApp
+ *
+ * @author Florian Popa <florian@webgenerals.com>
+ */
+public class SnippetsApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
